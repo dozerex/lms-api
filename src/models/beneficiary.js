d7573b2f615f6055c0bab7398c340df02ed2442b
Yuvarajan <yuvigopi@outlook.com>
2022-10-06 15:10:46 +0530

    New statics to check canLend

--- src/models/beneficiary.js
+++ src/models/beneficiary.js
@@ -51,6 +51,14 @@ const BeneficiarySchema = new mongoose.Schema({
     }]
 })
 
+BeneficiarySchema.statics.canLend = function(enrollmentNumber) {
+    return this.findOne({
+        enrollmentNumber,
+        booksLent: {$exists:true}, 
+        $where:"obj.booksLent.length<5"
+    })
+}
+
 
 const Beneficiary = mongoose.model('Beneficiary',BeneficiarySchema)
 
