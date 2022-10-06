80b882aa9b13b086511b12d2dd32b2313eee5ae2
Yuvarajan <yuvigopi@outlook.com>
2022-10-06 18:27:42 +0530

    Router issue-book

--- src/routers/book.js
+++ src/routers/book.js
@@ -57,51 +57,76 @@ bookRouter.get('/book-available/',async (req,res)=>{
     }
 })
 
-bookRouter.use('/issue-book/', async (req,res,next) => {
-    const {accessionNumber} = req.body
-    try {
-        const book = await BookStatus.findOne({
-            accessionNumber,
-            available: true
-        })
-        req.isAvaialable = false
-        if(book) {
-            req.isAvaialable = true
-        }
-        next()
-    } catch(e) {
-        res.status(400).send("Invalid Accession Number")
-    }
-})
+// bookRouter.use('/issue-book/', async (req,res,next) => {
+//     const {accessionNumber} = req.body
+//     try {
+//         const book = await BookStatus.findOne({
+//             accessionNumber,
+//             available: true
+//         })
+//         req.isAvaialable = false
+//         if(book) {
+//             req.isAvaialable = true
+//         }
+//         next()
+//     } catch(e) {
+//         res.status(400).send("Invalid Accession Number")
+//     }
+// })
 
-bookRouter.use('/issue-book/', async (req,res,next) => {
-    const {enrollmentNumber} = req.body
-    try {
-        const beneficiary = await Beneficiary.findOne({enrollmentNumber})
-        const noOfBooksLent = beneficiary.booksLent.length
-        req.canCheckOut = true
-        if(noOfBooksLent>=5) {
-            req.canCheckOut = false
-        }
-        next()
-    } catch(e) {
-        res.status(400).send("Invalid Enrollment Number")
-    }
-})
+// bookRouter.use('/issue-book/', async (req,res,next) => {
+//     const {enrollmentNumber} = req.body
+//     try {
+//         const beneficiary = await Beneficiary.findOne({enrollmentNumber})
+//         const noOfBooksLent = beneficiary.booksLent.length
+//         req.canCheckOut = true
+//         if(noOfBooksLent>=5) {
+//             req.canCheckOut = false
+//         }
+//         next()
+//     } catch(e) {
+//         res.status(400).send("Invalid Enrollment Number")
+//     }
+// })
 
 bookRouter.post('/issue-book/', async (req,res) => {
-    const {enrollmentNumber} = req.body
-    if(!req.isAvaialable) {
-        res.status(400).send("Book not available")
-    }
-    else if(!req.canCheckOut) {
-        res.status(400).send("Max Books lent")
+    const {accessionNumber,enrollmentNumber} = req.body
+    let book,beneficiary
+    try {  
+        book = await BookStatus.isAvailable(accessionNumber)
+        if(!book) {
+            throw new Error()
+        }
+    } catch(e) {
+        console.log(e)
+        return res.status(400).send("Book not available")
     }
     try {
-        const benificiary = Beneficiary.findOne({enrollmentNumber})
-        
+        beneficiary = await Beneficiary.findOne({enrollmentNumber})
+        if(beneficiary.booksLent.length>=5) {
+            throw new Error()
+        }
     } catch(e) {
-
+        console.log(e)
+        return res.status(400).send("Cant lend book")
+    }
+    try {
+        const mainBook = await Book.findOne({_id:book.book})
+        await Book.updateOne(mainBook,{$set:{
+            available:(mainBook.available-1)
+        }})
+        beneficiary.booksLent.push(book._id)
+        await Beneficiary.updateOne({_id:beneficiary._id},{$set:{
+            booksLent:booksLent
+        }})
+        await BookStatus.updateOne(book,{$set:{
+            available: false,
+            issueDate: Date(Date.now()),
+            dueDate: req.body.dueDate
+        }})
+        return res.status(201).send("Done")
+    } catch(e) {
+        res.status(400).send("Updation failed")
     }
 })
 
