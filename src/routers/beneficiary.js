e209147a84b8138c19afc9ed0c179a3a429b326e
Yuvarajan <yuvigopi@outlook.com>
2022-10-07 10:12:08 +0530

    Add books-lent by user route

--- src/routers/beneficiary.js
+++ src/routers/beneficiary.js
@@ -1,7 +1,8 @@
-const express = require('express')
+const express = require('express');
 
 //Model
-const Beneficiary = require('../models/beneficiary')
+const Beneficiary = require('../models/beneficiary');
+const BookStatus = require('../models/bookStatus');
 
 const beneficiaryRouter = express.Router({
     strict: true
@@ -21,6 +22,20 @@ beneficiaryRouter.post('/insert/',(req,res)=> {
     })
 })
 
-// beneficiaryRouter.get('/no-of-books-lent/',)
+
+beneficiaryRouter.post('/books-lent/', async (req, res) => {
+    try {
+        const beneficiary = await Beneficiary.findOne(req.body);
+        const booksLent = beneficiary.booksLent;
+        const books = await BookStatus.find({
+            _id : {$in: booksLent}
+        })
+        res.send(books)
+    } catch(e) {
+        console.log("error")
+        return res.status(400).send("No Beneficiary")
+    }
+})
+
 
 module.exports = beneficiaryRouter
